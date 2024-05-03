f9ee1324b5485cef2bc1517dfa1dccf1b5060b3e
Suaib Saifi <suaeb2001@gmail.com>
2024-05-03 19:16:24 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "Front End Developer",
+    "UI Engineer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
